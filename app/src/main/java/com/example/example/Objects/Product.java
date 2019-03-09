b6703a743ff32ca6a69b98b7135fcb073a0e59d8
ivanfermena <ivanfermena@gmail.com>
2019-03-09 13:06:14 +0100

    Acabados los test manuales de firebase

--- app/src/main/java/com/example/example/Objects/Product.java
+++ app/src/main/java/com/example/example/Objects/Product.java
@@ -1,24 +1,12 @@
 package com.example.example.Objects;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.widget.Toast;
-
-import com.example.example.Activities.MainActivity;
-import com.example.example.Activities.ProductActivity;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.EventListener;
 import java.util.List;
-
-import static com.example.example.Activities.MainActivity.OBJ;
 
 public class Product implements Serializable {
 
@@ -76,34 +64,25 @@ public class Product implements Serializable {
         database.child(String.valueOf(p.getBarcode())).setValue(p);
     }
 
+    public void modify(Product newProduct){
+        Product p = new Product(this.name, this.barcode, this.ingredients, this.adaptedFor);
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference("Productos");
+        database.child(String.valueOf(p.getBarcode())).setValue(newProduct);
+    }
+
     public void drop(){
         DatabaseReference database = FirebaseDatabase.getInstance().getReference("Productos");
         database.child(String.valueOf(this.getBarcode())).removeValue();
     }
 
-    public Product read() {
+    public void read(ValueEventListener eventListener) {
         DatabaseReference database = FirebaseDatabase.getInstance().getReference("Productos");
         Query q = database.orderByChild("barcode").equalTo(this.barcode);
-
-        list = new ArrayList<Product>();
-
-        ValueEventListener eventListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                list.clear();
-                if(dataSnapshot.exists()) {
-                    for (DataSnapshot d : dataSnapshot.getChildren()) {
-                        list.add(d.getValue(Product.class));
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        };
         q.addListenerForSingleValueEvent(eventListener);
-        return list.get(0);
+    }
+
+    @Override
+    public String toString() {
+        return "-" + this.name + " - Barcode:" + this.getBarcode()+", "+getIngredients()+ ", "+getAdaptedFor().toString()+"\n\n";
     }
 }
--- app/src/main/java/com/example/firebase/test/TestActivity.java
+++ app/src/main/java/com/example/firebase/test/TestActivity.java
@@ -1,33 +1,64 @@
 package com.example.firebase.test;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.example.Objects.Product;
 import com.example.example.R;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class TestActivity extends AppCompatActivity {
 
-    DatabaseReference database;
-
+    List<Product> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.support_simple_spinner_dropdown_item);
 
+        list = new ArrayList<>();
+
         //testCreateProduct();
 
         //testDropProduct(20182019);
 
-        // NO FUNCIONA
         //testShowProduct(20182019);
+
+        //testModifyProduct(20182019, new Product("Moyonesa Vegana", 20182019, Arrays.asList("Huevo", "Agua", "Maiz", "Azucar"), Arrays.asList("Vegetariano", "Celiaco", "Vegano")));
+
     }
+
+    ValueEventListener eventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            list.clear();
+            if(dataSnapshot.exists()) {
+                for (DataSnapshot d : dataSnapshot.getChildren()) {
+                    list.add(d.getValue(Product.class));
+                }
+            }
+            else{
+                Toast.makeText(getApplicationContext(),
+                        "The product doesn't exist", Toast.LENGTH_SHORT).show();
+            }
+            Log.i("PRODUCT", list.get(0).toString());
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+
+    };
 
     private void testCreateProduct(){
         Product p = new Product("Moyonesa", 20182019, Arrays.asList("Huevo", "Agua", "Maiz", "Azucar"), Arrays.asList("Vegetariano", "Celiaco"));
@@ -41,8 +72,12 @@ public class TestActivity extends AppCompatActivity {
 
     private void testShowProduct(Integer key){
         Product p = new Product(null, key, null, null);
-        Product prt = p.read();
-        p.toString();
+        p.read(eventListener);
+    }
+
+    private void testModifyProduct(Integer key, Product product){
+        Product p = new Product(null, key, null, null);
+        p.modify(product);
     }
 
 
