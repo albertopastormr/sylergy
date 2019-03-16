63372703f84fec02905edd872ad0eee686ccc5a4
AlexCabezas2018 <alexcabezas0705@gmail.com>
2019-03-16 09:51:35 +0100

    Eliminado version anterior

--- README.md
+++
@@ -1,5 +0,0 @@
-![](logo.png)
-
-[![Build Status](https://travis-ci.org/albertopastormr/sylergy.svg?branch=master)](https://travis-ci.org/albertopastormr/sylergy)
-
-# Sylergy
--- app/src/androidTest/java/com/example/example/test/Cucumber.java
+++
@@ -1,9 +0,0 @@
-package com.example.example.test;
-
-
-import cucumber.api.CucumberOptions;
-
-@CucumberOptions(features = "features",
-                 glue = {"com.example.example.test"})
-public class Cucumber {
-}
