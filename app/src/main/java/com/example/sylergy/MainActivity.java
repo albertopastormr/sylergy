611040365ce1932ac40fe70f247bb4b91017d485
AlexCabezas2018 <alexcabezas0705@gmail.com>
2019-03-16 09:57:46 +0100

    Version inicial

---
+++ app/src/main/java/com/example/sylergy/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.sylergy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
