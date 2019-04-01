08b4adc2db0f8c18f73eb917f6592222c59201dd
danie <dagonz09@ucm.es>
2019-04-01 17:53:31 +0200

    La busqueda por nombre completa tarda alrededor de 20 sec

--- app/src/main/java/com/example/sylergy/integration/product/dao/imp/DAOProductImp.java
+++ app/src/main/java/com/example/sylergy/integration/product/dao/imp/DAOProductImp.java
@@ -57,8 +57,66 @@ public class DAOProductImp implements DAOProduct { ;
 
         return listProducts.get(0);
     }
-
     @Override
+    public Product readByName(final Context context) {
+        //We will store the products in this list.
+        final List<Product> productList = new ArrayList<>();
+        productList.add(null);
+
+        //This is the name of the product we want to find in the database
+        final String nameToFindSimilar = (String)context.getData();
+
+        final String nameToFindSimilarLower = nameToFindSimilar.toLowerCase();
+        DatabaseReference databaseReference = FirebaseUtil.getSpecifiedReference("Products");
+        //This is a reference to our Firebase Database.
+        Query findByNameQuery = databaseReference.orderByChild("name")
+                .startAt("*" + nameToFindSimilar) // With that the query is faster
+                .endAt(nameToFindSimilar + "\uf8ff"); //We create the specific query*/
+
+        findByNameQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                productList.clear();
+                Context newContext = null;
+                if(dataSnapshot.exists()) {
+                    for (DataSnapshot d : dataSnapshot.getChildren()) { //We recolect our data
+
+                        String resultLower = d.getValue(Product.class).getName().toLowerCase();
+                        if (resultLower.contains(nameToFindSimilarLower))
+                            productList.add(d.getValue(Product.class));
+
+                    }
+
+                    if(context.getEvent().equals(Events.SEARCH_PRODUCT_NAME)) {
+                        newContext = new Context(Events.SEARCH_PRODUCT_NAME_OK, productList);
+                        newContext.setActivity(context.getActivity());
+                    }
+                    else if(context.getEvent().equals(Events.SEARCH_PRODUCT_BARCODE)){
+                        newContext = new Context(Events.SEARCH_PRODUCT_BARCODE_OK, productList);
+                        newContext.setActivity(context.getActivity());
+
+                    }
+                }
+                else {
+                    if(context.getEvent().equals(Events.SEARCH_PRODUCT_NAME)) {
+                        newContext = new Context(Events.SEARCH_PRODUCT_NAME_ERROR, null);
+                        newContext.setActivity(context.getActivity());
+                    }
+                    else if(context.getEvent().equals(Events.SEARCH_PRODUCT_BARCODE)){
+                        newContext = new Context(Events.SEARCH_PRODUCT_BARCODE_ERROR, productList);
+                        newContext.setActivity(context.getActivity());
+                    }
+                }
+                Presenter.getInstance().dispatchActivity(newContext);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) { }
+        });
+
+        return productList.get(0);
+    }
+    /*@Override
     public Product readByName(final Context context) {
         //We will store the products in this list.
         final List<Product> productList = new ArrayList<>();
@@ -112,6 +170,6 @@ public class DAOProductImp implements DAOProduct { ;
         });
 
         return productList.get(0);
-    }
+    }*/
 
 }
