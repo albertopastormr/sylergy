749ef835500662fb715c7c86c6fa25fdcf953e69
CurryRice233 <nmeia@qq.com>
2019-04-16 20:42:34 +0200

    barcode scan API

--- app/src/main/java/com/example/sylergy/fragments/CameraFragment.java
+++ app/src/main/java/com/example/sylergy/fragments/CameraFragment.java
@@ -1,9 +1,52 @@
 package com.example.sylergy.fragments;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.sylergy.R;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class CameraFragment extends Fragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_camara, container, false);
+        onScanBarcode();
+        return view;
+    }
+
+    public void onScanBarcode(){
+        IntentIntegrator integrator = IntentIntegrator.forSupportFragment(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
+        integrator.setPrompt("Scan Barcode");
+        integrator.setCameraId(0); // camera default
+        integrator.setBeepEnabled(false); // sound
+
+        integrator.initiateScan();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this.getActivity(), "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this.getActivity(), "Scanned Result=" + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+    }
 
 
 }
