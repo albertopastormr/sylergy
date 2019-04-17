b79298a6501f4262b04a1e75ca77115ab9e64b1d
ivanfermena <ivanfermena@gmail.com>
2019-04-17 11:26:30 +0200

    Intento de correcion de test de camara y nuevo test

--- app/src/androidTest/java/com/example/sylergy/activities/ActivityCameraEspressoTest.java
+++ app/src/androidTest/java/com/example/sylergy/activities/ActivityCameraEspressoTest.java
@@ -1,5 +1,7 @@
 package com.example.sylergy.activities;
 
+import android.provider.MediaStore;
+
 import com.example.sylergy.R;
 import com.example.sylergy.activities.utils.EspressoUtils;
 import com.example.sylergy.activities.utils.ToastMatcher;
@@ -26,11 +28,14 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -44,8 +49,17 @@ public class ActivityCameraEspressoTest {
     public void cameraButtonUITest() throws InterruptedException {
         onView(allOf(withText("Barcode"),isDescendantOfA(withId(R.id.bottomNavigationView)),isDisplayed())).perform(click());
         Espresso.pressBack();
+        Thread.sleep(750);
         onView(withText("Cancelled")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
     }
+
+    // Test camera active
+
+    /**@Test
+    public void cameraCorrectBarcodeUITest() throws InterruptedException {
+        onView(allOf(withText("Barcode"),isDescendantOfA(withId(R.id.bottomNavigationView)),isDisplayed())).perform(click());
+        intended(hasAction(equalTo(MediaStore.ACTION_IMAGE_CAPTURE)));
+    }*/
 
     @Before
     public void registerActivity(){
