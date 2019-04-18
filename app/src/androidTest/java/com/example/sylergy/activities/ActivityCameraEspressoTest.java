0f1b21a2d6a570dafdadb6952e32a924e3c3d608
CurryRice233 <nmeia@qq.com>
2019-04-18 19:32:36 +0200

    change scan barcode to main fragment and test

--- app/src/androidTest/java/com/example/sylergy/activities/ActivityCameraEspressoTest.java
+++ app/src/androidTest/java/com/example/sylergy/activities/ActivityCameraEspressoTest.java
@@ -1,11 +1,6 @@
 package com.example.sylergy.activities;
 
-import android.provider.MediaStore;
-
 import com.example.sylergy.R;
-import com.example.sylergy.activities.utils.EspressoUtils;
-import com.example.sylergy.activities.utils.ToastMatcher;
-import com.example.sylergy.logs.Logs;
 
 import org.junit.After;
 import org.junit.Before;
@@ -16,26 +11,17 @@ import org.junit.runner.RunWith;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.IdlingResource;
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
-import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.allOf;
-import static org.hamcrest.CoreMatchers.equalTo;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -46,10 +32,11 @@ public class ActivityCameraEspressoTest {
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void cameraButtonUITest() throws InterruptedException {
-        onView(allOf(withText("Barcode"),isDescendantOfA(withId(R.id.bottomNavigationView)),isDisplayed())).perform(click());
+    public void cameraButtonUITest(){
+        onView(withId(R.id.btnCamera)).perform(click());
         Espresso.pressBack();
-        onView(withText("Cancelled")).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
+        onView(allOf(withText("Home"),isDescendantOfA(withId(R.id.bottomNavigationView)),isDisplayed()));
+
     }
 
     // Test camera active
