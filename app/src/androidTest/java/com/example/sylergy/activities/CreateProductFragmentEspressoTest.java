d051586aa83e52a394856c6e6997647d5e231eb4
Matteo Isnenghi <misnengh@ucm.es>
2019-04-24 21:07:52 +0200

    Created createProductFragment ui

---
+++ app/src/androidTest/java/com/example/sylergy/activities/CreateProductFragmentEspressoTest.java
@@ -0,0 +1,286 @@
+package com.example.sylergy.activities;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.example.sylergy.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class CreateProductFragmentEspressoTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void createProductFragmentEspressoTest() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction bottomNavigationItemView = onView(
+                allOf(withId(R.id.navigation_new_product), withContentDescription("New Product"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.bottomNavigationView),
+                                        0),
+                                2),
+                        isDisplayed()));
+        bottomNavigationItemView.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction editText = onView(
+                allOf(withId(R.id.editText_productName), withText("Name of the product"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
+                                        0),
+                                2),
+                        isDisplayed()));
+        editText.check(matches(withText("Name of the product")));
+
+        ViewInteraction editText2 = onView(
+                allOf(withId(R.id.editText_productBarcode), withText("Barcode of the product"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
+                                        0),
+                                3),
+                        isDisplayed()));
+        editText2.check(matches(withText("Barcode of the product")));
+
+        ViewInteraction imageView = onView(
+                allOf(withId(R.id.productImage),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
+                                        0),
+                                1),
+                        isDisplayed()));
+        imageView.check(matches(isDisplayed()));
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.button_uploadPicture),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
+                                        0),
+                                4),
+                        isDisplayed()));
+        button.check(matches(isDisplayed()));
+
+        ViewInteraction button2 = onView(
+                allOf(withId(R.id.button_scanBarcode),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
+                                        0),
+                                5),
+                        isDisplayed()));
+        button2.check(matches(isDisplayed()));
+
+        ViewInteraction button3 = onView(
+                allOf(withId(R.id.button_addNutrients),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
+                                        0),
+                                7),
+                        isDisplayed()));
+        button3.check(matches(isDisplayed()));
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.nutrimentsTitle), withText("Nutrients:"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
+                                        0),
+                                6),
+                        isDisplayed()));
+        textView.check(matches(withText("Nutrients:")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.titleIngredients), withText("Ingredients:"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
+                                        0),
+                                9),
+                        isDisplayed()));
+        textView2.check(matches(withText("Ingredients:")));
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.selectedNutrients),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
+                                        0),
+                                8),
+                        isDisplayed()));
+        textView3.check(matches(isDisplayed()));
+
+        ViewInteraction editText3 = onView(
+                allOf(withId(R.id.ingredients), withText("Please input the ingredients of this product..."),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
+                                        0),
+                                10),
+                        isDisplayed()));
+        editText3.check(matches(withText("Please input the ingredients of this product...")));
+
+        ViewInteraction editText4 = onView(
+                allOf(withId(R.id.ingredients), withText("Please input the ingredients of this product..."),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
+                                        0),
+                                10),
+                        isDisplayed()));
+        editText4.check(matches(withText("Please input the ingredients of this product...")));
+
+        ViewInteraction imageButton = onView(
+                allOf(withId(R.id.button_checkNewProduct),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
+                                        0),
+                                11),
+                        isDisplayed()));
+        imageButton.check(matches(isDisplayed()));
+
+        ViewInteraction imageButton2 = onView(
+                allOf(withId(R.id.button_checkNewProduct),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
+                                        0),
+                                11),
+                        isDisplayed()));
+        imageButton2.check(matches(isDisplayed()));
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.editText_productName),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                1)));
+        appCompatEditText.perform(scrollTo(), replaceText("hola"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.editText_productName), withText("hola"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                1)));
+        appCompatEditText2.perform(pressImeActionButton());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.editText_productBarcode),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                2)));
+        appCompatEditText3.perform(scrollTo(), click());
+
+        ViewInteraction appCompatEditText4 = onView(
+                allOf(withId(R.id.editText_productBarcode),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                2)));
+        appCompatEditText4.perform(scrollTo(), replaceText("123"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText5 = onView(
+                allOf(withId(R.id.editText_productBarcode), withText("123"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                2)));
+        appCompatEditText5.perform(pressImeActionButton());
+
+        ViewInteraction appCompatEditText6 = onView(
+                allOf(withId(R.id.ingredients),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                10)));
+        appCompatEditText6.perform(scrollTo(), replaceText("ingrediente1"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText7 = onView(
+                allOf(withId(R.id.ingredients), withText("ingrediente1"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                10)));
+        appCompatEditText7.perform(pressImeActionButton());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
--- app/src/main/java/com/example/sylergy/fragments/CreateProductFragment.java
+++ app/src/main/java/com/example/sylergy/fragments/CreateProductFragment.java
@@ -1,11 +1,17 @@
 package com.example.sylergy.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.sylergy.R;
 import com.example.sylergy.activities.UpdateActivity;
@@ -13,18 +19,122 @@ import com.example.sylergy.logs.LogException;
 
 import com.example.sylergy.objects.Context;
 
+import java.util.ArrayList;
+
 public class CreateProductFragment extends Fragment implements UpdateActivity {
+
+    ImageView productImage;
+    EditText productName;
+    EditText productBarcode;
+
+    Button uploadPicture;
+    Button scanBarcode;
+
+    Button button_addNutriments;
+    TextView nutriments;
+    String[] listNutriments;
+    boolean[] checkedNutriments;
+    ArrayList<Integer> selectedNutriments;
+
+    EditText ingredients;
+
+    Button checkNewProduct;
+
+
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_create_product, container, false);
 
+
+        nutriments = view.findViewById(R.id.selectedNutrients);
+        productImage = view.findViewById(R.id.productImage);
+        productName = view.findViewById(R.id.editText_productName);
+        productBarcode = view.findViewById(R.id.editText_productBarcode);
+        uploadPicture = view.findViewById(R.id.button_uploadPicture);
+        scanBarcode = view.findViewById(R.id.button_scanBarcode);
+
+        button_addNutriments =  view.findViewById(R.id.button_addNutrients);
+        listNutriments = getResources().getStringArray(R.array.nutrients);
+        checkedNutriments = new boolean[listNutriments.length];
+        selectedNutriments = new ArrayList<>();
+
+        ingredients = view.findViewById(R.id.ingredients);
+
+       // checkNewProduct = view.findViewById(R.id.button_checkNewProduct)
+
+        button_addNutriments.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle((R.string.select_nutrients_title));
+                builder.setMultiChoiceItems(listNutriments, checkedNutriments, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int position, boolean isChecked) {
+                        if (isChecked) {
+                            if (!selectedNutriments.contains(position)) {
+                                selectedNutriments.add(position);
+                            }
+                        } else  if (selectedNutriments.contains(position)){
+
+                            selectedNutriments.remove(selectedNutriments.indexOf(position));
+                        }
+                    }
+                });
+
+                    builder.setCancelable(false);
+                    builder.setPositiveButton((R.string.ok), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String alergeno = "";
+                            for (int i = 0; i < selectedNutriments.size(); i++) {
+                                if (i < selectedNutriments.size()-1) {
+                                    alergeno = alergeno + listNutriments[selectedNutriments.get(i)] + ", ";
+                                } else {
+                                    alergeno = alergeno + listNutriments[selectedNutriments.get(i)];
+                                }
+
+                            }
+                            nutriments.setText(alergeno);
+                        }
+
+                    });
+
+                    builder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+
+                    builder.setNeutralButton("Clear all", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            for (int i = 0; i < checkedNutriments.length; i++) {
+                                checkedNutriments[i] = false;
+                                selectedNutriments.clear();
+                                nutriments.setText("");
+
+                            }
+                        }
+                    });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+
+
+
+
         return view;
     }
 
     @Override
     public void updateWithCommandResult(Context context) throws LogException {
+
 
     }
 }
