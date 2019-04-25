8d22d44fb6eae11168524b06af368acb4d7b9b2c
Matteo Isnenghi <misnengh@ucm.es>
2019-04-25 11:25:50 +0200

    Fixed ActivityCreateProductEspressoTest

--- app/src/androidTest/java/com/example/sylergy/activities/ActivityCreateProductEspressoTest.java
+++ app/src/androidTest/java/com/example/sylergy/activities/ActivityCreateProductEspressoTest.java
@@ -1,10 +1,10 @@
 package com.example.sylergy.activities;
 
 
-import android.support.test.espresso.ViewInteraction;
-import android.support.test.filters.LargeTest;
-import android.support.test.rule.ActivityTestRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
@@ -19,20 +19,14 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
-import static android.support.test.espresso.action.ViewActions.replaceText;
-import static android.support.test.espresso.action.ViewActions.scrollTo;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
-import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -42,7 +36,7 @@ public class ActivityCreateProductEspressoTest {
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void createProductFragmentEspressoTest() {
+    public void activityCreateProductEspressoTest() {
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
@@ -62,6 +56,59 @@ public class ActivityCreateProductEspressoTest {
                         isDisplayed()));
         bottomNavigationItemView.perform(click());
 
+       /* ViewInteraction textView = onView(
+                allOf(withId(R.id.createProductTitle), withText("Create product"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("Create product"))); */
+
+       ViewInteraction textView = onView(allOf(withId(R.id.createProductTitle)));
+       textView.check(matches(withText("Create product")));
+
+        ViewInteraction imageView = onView(allOf(withId(R.id.productImage)));
+        imageView.check(matches(isDisplayed()));
+
+        ViewInteraction editText = onView(allOf(withId(R.id.editText_productName)));
+
+
+        editText.check(matches(isDisplayed()));
+
+        ViewInteraction editText2 = onView(
+                allOf(withId(R.id.editText_productBarcode)));
+        editText2.check(matches(isDisplayed()));
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.button_uploadPicture)));
+        button.check(matches(isDisplayed()));
+
+        ViewInteraction button2 = onView(
+                allOf(withId(R.id.button_scanBarcode)));
+        button2.check(matches(isDisplayed()));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.nutrimentsTitle)));
+        textView2.check(matches(withText("Nutrients:")));
+
+        ViewInteraction button3 = onView(
+                allOf(withId(R.id.button_addNutrients)));
+        button3.check(matches(isDisplayed()));
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.selectedNutrients)));
+        textView3.check(matches(withText("")));
+
+        ViewInteraction textView4 = onView(
+                allOf(withId(R.id.titleIngredients)));
+        textView4.check(matches(withText("Ingredients:")));
+
+        ViewInteraction editText3 = onView(
+                allOf(withId(R.id.ingredients)));
+        editText3.check(matches(isDisplayed()));
+
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
@@ -71,198 +118,7 @@ public class ActivityCreateProductEspressoTest {
             e.printStackTrace();
         }
 
-        ViewInteraction editText = onView(
-                allOf(withId(R.id.editText_productName), withText("Name of the product"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
-                                        0),
-                                2),
-                        isDisplayed()));
-        editText.check(matches(withText("Name of the product")));
 
-        ViewInteraction editText2 = onView(
-                allOf(withId(R.id.editText_productBarcode), withText("Barcode of the product"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
-                                        0),
-                                3),
-                        isDisplayed()));
-        editText2.check(matches(withText("Barcode of the product")));
-
-        ViewInteraction imageView = onView(
-                allOf(withId(R.id.productImage),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
-                                        0),
-                                1),
-                        isDisplayed()));
-        imageView.check(matches(isDisplayed()));
-
-        ViewInteraction button = onView(
-                allOf(withId(R.id.button_uploadPicture),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
-                                        0),
-                                4),
-                        isDisplayed()));
-        button.check(matches(isDisplayed()));
-
-        ViewInteraction button2 = onView(
-                allOf(withId(R.id.button_scanBarcode),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
-                                        0),
-                                5),
-                        isDisplayed()));
-        button2.check(matches(isDisplayed()));
-
-        ViewInteraction button3 = onView(
-                allOf(withId(R.id.button_addNutrients),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
-                                        0),
-                                7),
-                        isDisplayed()));
-        button3.check(matches(isDisplayed()));
-
-        ViewInteraction textView = onView(
-                allOf(withId(R.id.nutrimentsTitle), withText("Nutrients:"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
-                                        0),
-                                6),
-                        isDisplayed()));
-        textView.check(matches(withText("Nutrients:")));
-
-        ViewInteraction textView2 = onView(
-                allOf(withId(R.id.titleIngredients), withText("Ingredients:"),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
-                                        0),
-                                9),
-                        isDisplayed()));
-        textView2.check(matches(withText("Ingredients:")));
-
-        ViewInteraction textView3 = onView(
-                allOf(withId(R.id.selectedNutrients),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
-                                        0),
-                                8),
-                        isDisplayed()));
-        textView3.check(matches(isDisplayed()));
-
-        ViewInteraction editText3 = onView(
-                allOf(withId(R.id.ingredients), withText("Please input the ingredients of this product..."),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
-                                        0),
-                                10),
-                        isDisplayed()));
-        editText3.check(matches(withText("Please input the ingredients of this product...")));
-
-        ViewInteraction editText4 = onView(
-                allOf(withId(R.id.ingredients), withText("Please input the ingredients of this product..."),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
-                                        0),
-                                10),
-                        isDisplayed()));
-        editText4.check(matches(withText("Please input the ingredients of this product...")));
-
-        ViewInteraction imageButton = onView(
-                allOf(withId(R.id.button_checkNewProduct),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
-                                        0),
-                                11),
-                        isDisplayed()));
-        imageButton.check(matches(isDisplayed()));
-
-        ViewInteraction imageButton2 = onView(
-                allOf(withId(R.id.button_checkNewProduct),
-                        childAtPosition(
-                                childAtPosition(
-                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
-                                        0),
-                                11),
-                        isDisplayed()));
-        imageButton2.check(matches(isDisplayed()));
-
-        ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.editText_productName),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.ScrollView")),
-                                        0),
-                                1)));
-        appCompatEditText.perform(scrollTo(), replaceText("hola"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText2 = onView(
-                allOf(withId(R.id.editText_productName), withText("hola"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.ScrollView")),
-                                        0),
-                                1)));
-        appCompatEditText2.perform(pressImeActionButton());
-
-        ViewInteraction appCompatEditText3 = onView(
-                allOf(withId(R.id.editText_productBarcode),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.ScrollView")),
-                                        0),
-                                2)));
-        appCompatEditText3.perform(scrollTo(), click());
-
-        ViewInteraction appCompatEditText4 = onView(
-                allOf(withId(R.id.editText_productBarcode),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.ScrollView")),
-                                        0),
-                                2)));
-        appCompatEditText4.perform(scrollTo(), replaceText("123"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText5 = onView(
-                allOf(withId(R.id.editText_productBarcode), withText("123"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.ScrollView")),
-                                        0),
-                                2)));
-        appCompatEditText5.perform(pressImeActionButton());
-
-        ViewInteraction appCompatEditText6 = onView(
-                allOf(withId(R.id.ingredients),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.ScrollView")),
-                                        0),
-                                10)));
-        appCompatEditText6.perform(scrollTo(), replaceText("ingrediente1"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText7 = onView(
-                allOf(withId(R.id.ingredients), withText("ingrediente1"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.ScrollView")),
-                                        0),
-                                10)));
-        appCompatEditText7.perform(pressImeActionButton());
     }
 
     private static Matcher<View> childAtPosition(
