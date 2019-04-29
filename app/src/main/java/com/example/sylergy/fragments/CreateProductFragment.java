407d0ea883dc37ecfeacd2fd37d3a90e709b2ba5
Matteo Isnenghi <misnengh@ucm.es>
2019-04-29 20:10:49 +0200

    Updated CreateProductFragment, now UPLOAD PICTURE works.

--- app/src/main/java/com/example/sylergy/fragments/CreateProductFragment.java
+++ app/src/main/java/com/example/sylergy/fragments/CreateProductFragment.java
@@ -2,7 +2,12 @@ package com.example.sylergy.fragments;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -28,6 +33,8 @@ import com.example.sylergy.objects.Product;
 import com.example.sylergy.presenter.Presenter;
 import com.google.firebase.database.DatabaseException;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -42,6 +49,9 @@ public class CreateProductFragment extends Fragment implements UpdateActivity {
     EditText productBarcode;
 
     Button uploadPicture;
+    private static final int PICK_IMAGE = 100;
+    private static final int UPLOAD_IMAGE = 101;
+    Uri imageUri;
     Button scanBarcode;
 
     Button button_addNutriments;
@@ -53,8 +63,6 @@ public class CreateProductFragment extends Fragment implements UpdateActivity {
     EditText ingredients;
 
     ImageButton checkNewProduct;
-
-
 
 
     @Nullable
@@ -70,7 +78,7 @@ public class CreateProductFragment extends Fragment implements UpdateActivity {
         uploadPicture = view.findViewById(R.id.button_uploadPicture);
         scanBarcode = view.findViewById(R.id.button_scanBarcode);
 
-        button_addNutriments =  view.findViewById(R.id.button_addNutrients);
+        button_addNutriments = view.findViewById(R.id.button_addNutrients);
         listNutriments = getResources().getStringArray(R.array.nutrients);
         checkedNutriments = new boolean[listNutriments.length];
         selectedNutriments = new ArrayList<>();
@@ -78,6 +86,37 @@ public class CreateProductFragment extends Fragment implements UpdateActivity {
         ingredients = view.findViewById(R.id.ingredients);
 
         checkNewProduct = view.findViewById(R.id.button_checkNewProduct);
+
+
+        uploadPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder pictureDialog = new AlertDialog.Builder(CreateProductFragment.this.getContext());
+                pictureDialog.setTitle("Select Action");
+                String[] pictureDialogItems = {
+                        "Select photo from gallery",
+                        "Capture photo from camera"};
+                pictureDialog.setItems(pictureDialogItems,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                switch (which) {
+                                    case 0:
+                                        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                                        startActivityForResult(gallery, PICK_IMAGE);
+                                        break;
+                                    case 1:
+                                        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                        startActivityForResult(camera, UPLOAD_IMAGE);
+                                        break;
+                                }
+                            }
+                        });
+                pictureDialog.show();
+            }
+        });
+
 
         button_addNutriments.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,49 +130,49 @@ public class CreateProductFragment extends Fragment implements UpdateActivity {
                             if (!selectedNutriments.contains(position)) {
                                 selectedNutriments.add(position);
                             }
-                        } else  if (selectedNutriments.contains(position)){
+                        } else if (selectedNutriments.contains(position)) {
 
                             selectedNutriments.remove(selectedNutriments.indexOf(position));
                         }
                     }
                 });
 
-                    builder.setCancelable(false);
-                    builder.setPositiveButton((R.string.ok), new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            String nutrients = "";
-                            for (int i = 0; i < selectedNutriments.size(); i++) {
-                                if (i < selectedNutriments.size()-1) {
-                                    nutrients = nutrients + listNutriments[selectedNutriments.get(i)] + ", ";
-                                } else {
-                                    nutrients = nutrients + listNutriments[selectedNutriments.get(i)];
-                                }
-
+                builder.setCancelable(false);
+                builder.setPositiveButton((R.string.ok), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String nutrients = "";
+                        for (int i = 0; i < selectedNutriments.size(); i++) {
+                            if (i < selectedNutriments.size() - 1) {
+                                nutrients = nutrients + listNutriments[selectedNutriments.get(i)] + ", ";
+                            } else {
+                                nutrients = nutrients + listNutriments[selectedNutriments.get(i)];
                             }
-                            nutriments.setText(nutrients);
+
                         }
+                        nutriments.setText(nutrients);
+                    }
 
-                    });
+                });
 
-                    builder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
+                builder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.setNeutralButton("Clear all", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        for (int i = 0; i < checkedNutriments.length; i++) {
+                            checkedNutriments[i] = false;
+                            selectedNutriments.clear();
+                            nutriments.setText("");
+
                         }
-                    });
-
-                    builder.setNeutralButton("Clear all", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            for (int i = 0; i < checkedNutriments.length; i++) {
-                                checkedNutriments[i] = false;
-                                selectedNutriments.clear();
-                                nutriments.setText("");
-
-                            }
-                        }
-                    });
+                    }
+                });
                 AlertDialog dialog = builder.create();
                 dialog.show();
             }
@@ -164,7 +203,7 @@ public class CreateProductFragment extends Fragment implements UpdateActivity {
                 //Convertir EditTest en List<String>
                 String ingredients1 = ingredients.getText().toString();
                 ArrayList<HashMap<String, Object>> arrayListIngredients = new ArrayList<HashMap<String, Object>>();
-                if(!ingredients1.isEmpty()) {
+                if (!ingredients1.isEmpty()) {
                     String[] ingredientsArray = ingredients1.split("\n");
 
                     List<String> ingredientsList = Arrays.asList(ingredientsArray);
@@ -179,27 +218,26 @@ public class CreateProductFragment extends Fragment implements UpdateActivity {
                 }
 
                 HashMap<String, Object> nutrientsMap = new HashMap<String, Object>();
-                for(int i : selectedNutriments){
+                for (int i : selectedNutriments) {
                     //AQUI DEBE IR EL ARGUMENTO DE LOS NUTRIENTES
-                    nutrientsMap.put(listNutriments[i],listNutriments[i]);
+                    nutrientsMap.put(listNutriments[i], listNutriments[i]);
                 }
 
 
                 Product product = null;
 
                 //Si pasa los criterios del formulario
-                if(applicationForm.checkName(productName.getText().toString(), getActivity()) &&
+                if (applicationForm.checkName(productName.getText().toString(), getActivity()) &&
                         applicationForm.checkBarcode(productBarcode.getText().toString(), getActivity()) &&
-                        applicationForm.checkIngredients(arrayListIngredients, getActivity())  ){
-                        //&& applicationForm.checkNutrients(productName.getText().toString(), getActivity())){
-                    product = new Product(productBarcode.getText().toString(),productImage.toString(),arrayListIngredients,
-                            productName.getText().toString(),nutrientsMap);
-                    Presenter.getInstance().action(new Context(Events.CREATE_PRODUCT,product,CreateProductFragment.this));
+                        applicationForm.checkIngredients(arrayListIngredients, getActivity())) {
+                    //&& applicationForm.checkNutrients(productName.getText().toString(), getActivity())){
+                    product = new Product(productBarcode.getText().toString(), productImage.toString(), arrayListIngredients,
+                            productName.getText().toString(), nutrientsMap);
+                    Presenter.getInstance().action(new Context(Events.CREATE_PRODUCT, product, CreateProductFragment.this));
 
                 }
             }
         });
-
 
 
         return view;
@@ -208,11 +246,44 @@ public class CreateProductFragment extends Fragment implements UpdateActivity {
 
     @Override
     public void updateWithCommandResult(Context context) throws LogException {
-        if(context.getEvent().compareToIgnoreCase(Events.CREATE_PRODUCT_OK) == 0){
-            Toast.makeText(MainActivity.context, ((Product)context.getData()).toString(), Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(MainActivity.context, (String)context.getData(), Toast.LENGTH_SHORT).show();
+        if (context.getEvent().compareToIgnoreCase(Events.CREATE_PRODUCT_OK) == 0) {
+            Toast.makeText(MainActivity.context, ((Product) context.getData()).toString(), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(MainActivity.context, (String) context.getData(), Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE) {
+            imageUri = data.getData();
+            try {
+                InputStream imageStream = this.getContext().getContentResolver().openInputStream(imageUri);
+                Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                selectedImage = getResizedBitmap(selectedImage, 250);// tamaño de la imagen
+                Toast.makeText(CreateProductFragment.this.getContext(), "Image Saved!", Toast.LENGTH_SHORT).show();
+                productImage.setImageBitmap(selectedImage);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        } else if (requestCode == UPLOAD_IMAGE) {
+            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+            productImage.setImageBitmap(thumbnail);
+            //saveImage(thumbnail);
+            Toast.makeText(CreateProductFragment.this.getContext(), "Image Saved!", Toast.LENGTH_SHORT).show();
         }
 
     }
+
+    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = maxSize;
+        int height = maxSize;
+        return Bitmap.createScaledBitmap(image, width, height, true);
+    }
+
 }
